1fb500d6b2dd6503ed49915a3be51409dddc3196
= <deadswallow@gmail.com>
2014-07-28 08:02:30 +0300

    remove unnecessary tests. Trying to do async tests

--- test/spec/services/levels.js
+++ test/spec/services/levels.js
@@ -13,6 +13,7 @@ describe('Service: levels', function () {
 
   it('should do something', function () {
     expect(!!levels).toBe(true);
+      expect( levels.get().then( function(d) {return false;} ) ).toBe(false);
   });
 
 });
