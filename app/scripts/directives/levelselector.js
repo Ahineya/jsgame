acbcd235727df2c5b624ac11983721ab5772a726
= <deadswallow@gmail.com>
2014-07-29 07:51:09 +0300

    Refactor: levelselector directive

--- app/scripts/directives/levelselector.js
+++ app/scripts/directives/levelselector.js
@@ -10,46 +10,71 @@ angular.module('jsgameApp')
   .directive('levelSelector', function () {
     return {
         templateUrl: '/views/levelSelector.html',
-        restrict: 'A',
+        restrict: 'EA',
         link: function postLink(scope, element, attrs) {
-            var CanvasBuild  = {
-                build : function(obj){
-                    var corners = 5;
-                    var cnv = obj.getContext('2d');
-                    cnv.beginPath();
-                    var oW = obj.width, oH = obj.height, radius = obj.width/2 - 5, y0 = obj.width/2, x0 = obj.width/2;
-                    console.log(oW);
-                    console.log(oH);
-                    //Begin
-                    cnv.moveTo(x0, y0);
-                    var  da = 360/corners, a= 0, path = [];
 
-                    for (var i =0; i < corners; i++){
+            function Polygon(options) {
+                this.options = {
+                    lineWidth: 2,
+                    color: 'green',
+                    corners: 6
+                };
+
+                angular.extend(this.options, options);
+
+                this.options.canvas = this.options.element.find('canvas')[0];
+
+                this.obj = this.options.canvas;
+                this.ctx = this.options.canvas.getContext('2d');
+
+                this.build = function() {
+                    this.ctx.beginPath();
+                    var oW = this.obj.width,
+                        oH = this.obj.height,
+                        radius = this.obj.width/2 - 5,
+                        y0 = this.obj.width/2,
+                        x0 = this.obj.width/2;
+
+                    this.ctx.moveTo(x0, y0);
+
+                    var da = 360/this.options.corners,
+                        a = 0,
+                        path = [];
+
+                    for (var i = 0; i < this.options.corners; i++){
                         var temp_x = (x0+Math.round(radius*Math.cos(a*Math.PI/180)));
                         var temp_y = (y0-Math.round(radius*Math.sin(a*Math.PI/180)));
-                        path.push([temp_x,temp_y])
-                        //console.log(path);
-                        if (i == 0) {
-                            cnv.moveTo(path[i][0], path[i][1]);
+                        path.push([temp_x,temp_y]);
+                        if (i === 0) {
+                            this.ctx.moveTo(path[i][0], path[i][1]);
                         } else {
-                            cnv.lineTo(path[i][0], path[i][1]);
+                            this.ctx.lineTo(path[i][0], path[i][1]);
                         }
                         a = a + da;
                     }
-                    cnv.lineTo(path[0][0], path[0][1]);
-                    // complete custom shape
-                    cnv.closePath();
-                    cnv.lineWidth = 2;
-                    cnv.strokeStyle = 'green';
-                    cnv.fill();
-                    cnv.stroke();
-                    return true;
-                }
+
+                    this.ctx.lineTo(path[0][0], path[0][1]);
+                    this.ctx.closePath();
+
+                    this.ctx.lineWidth = this.options.lineWidth;
+                    this.ctx.strokeStyle = this.options.color;
+
+                    this.ctx.fill();
+                    this.ctx.stroke();
+
+                };
+
+            }
+
+            var options = {
+                element: element,
+                color: attrs.strokeStyle,
+                corners: scope.$eval(attrs.corners),
+                lineWidth: attrs.lineWidth
             };
 
-            console.log(element.find('canvas'));
-
-            CanvasBuild.build(element.find('canvas')[0]);
+            var p = new Polygon(options);
+            p.build();
 
         }
     };
